fb3414a9e110524cb0702b1a852f8c1a5e93861b
afisher97 <acf@averycfisher.com>
2024-08-14 21:03:03 -0500

    Added changelog page

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 
+# Path to logs directory
+LOGS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'changelogs')
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/changelog_buildcraft')
+def changelog_buildcraft():
+    # Read the changelog file
+    changelog_file = os.path.join(LOGS_DIR, 'buildcraft_changelog.txt')
+    with open(changelog_file, 'r') as file:
+        changelog = file.readlines()
+
+    return render_template('changelog_buildcraft.html', changelog=changelog)
 
 
 if __name__ == '__main__':
